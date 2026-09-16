c7fc9e0324223ba259c3d18c6d26fb4d7592c31f
Atharv Mehta <atharvmehta7219@gmail.com>
2026-09-16 21:56:50 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
-int main()
-{
-int n;
-printf("Enter size of the array");
-scanf("%d",&n);
-int arr[n];
-int arr2[n+1];
-int i,j,p=0;
 
-printf("Enter elements of array ");
-for(i=0;i<n;i++)
-{
-    scanf("%d",&arr[i]);
-}
-int pos;
-printf("Enter position where element has to be inserted");
-scanf("%d",&pos);
-int ele;
-printf("Enter element to be inserted");
-scanf("%d",&ele);
+int main() {
+    int a[10], i;
+    int max, second_max;
 
-pos-=1;
+    printf("Enter array elements:\n");
 
-for(i=0;i<n;i++)
-{
-    if(p!=pos)
-    {
-        arr2[p]=arr[i];
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &a[i]);
     }
-    else{
-        arr2[p]=ele;
-       
-        --i;
-    }
-    ++p;
-}
 
-for(i=0;i<n+1;i++)
-{
-    printf("%d",arr2[i]);
-}
+
+    max = a[0];
+
+    for(i = 1; i < 10; i++) {
+        if(a[i] > max) {
+            max = a[i];
+        }
+    }
+
+  
+    second_max = a[0];
+
+    for(i = 0; i < 10; i++) {
+        if(a[i] > second_max && a[i] < max) {
+            second_max = a[i];
+        }
+    }
+
+    printf("Second maximum = %d\n", second_max);
+
+   
 }
