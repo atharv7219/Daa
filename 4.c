6fe00d874297dfe28befbe5e6fc6c802cc4762cd
Atharv Mehta <atharvmehta7219@gmail.com>
2026-09-16 21:58:14 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a[10], i;
+
+    printf("Enter array elements:");
+
+    for(i = 0; i < 10; i++)
+        scanf("%d", &a[i]);
+
+    printf("Array elements are:");
+
+    for(i = 0; i < 10; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
